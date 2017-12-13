0914fc06d5fbeebd5c1b15cb8cd29fc73d48ba32
Panida Wiriyachaiporn <neufii.s@gmail.com>
2017-12-13 19:52:08 +0700

    Change index msg

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
  
 @app.route('/')
 def index():
-    return 'This is chatbot server.'
+    return 'THIS IS NxNxN Chatbot.'
 @app.route('/bot', methods=['POST'])
 
 def bot():
